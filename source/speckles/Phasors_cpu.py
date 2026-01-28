d73eb68cd9ba106e335cf130d80d359d8953d05a
Gabriel Genest <9.genest.gabriel@gmail.com>
2026-01-28 17:09:19 -0500

    Fixed some returns and added some properties

--- source/speckles/Phasors_cpu.py
+++ source/speckles/Phasors_cpu.py
@@ -6,15 +6,35 @@ class Phasors:
 
     def __init__(self, complex_amplitudes: np.ndarray):
         # complex_amplitudes should be of shape (M, N, N) where M is the number of time steps.
+        shape = complex_amplitudes.shape
         self.__amps = complex_amplitudes
+        self.__M, self.__N1, self.__N2 = shape
+        self.__shape = (self.__M, self.__N1, self.__N2)
 
     @property
-    def amps(self):
+    def complex_amps(self) -> np.ndarray:
         return self.__amps
 
+    @property
+    def M(self) -> int:
+        return self.__M
+
+    @property
+    def N1(self) -> int:
+        return self.__N1
+
+    @property
+    def N2(self) -> int:
+        return self.__N2
+
+    @property
+    def shape(self) -> typing.Tuple[int, int, int]:
+        return self.__shape
+
     @classmethod
-    def from_correlation(cls, correlation_matrix: np.ndarray, N: int, rng_gen=None, seed=None, real_dtype=np.float32,
-                         complex_dtype=np.complex64) -> "Phasors":
+    def from_correlation(cls, correlation_matrix: np.ndarray, N: int, rng_gen: np.random.Generator = None,
+                         seed: int = None, real_dtype: np.dtype = np.float32,
+                         complex_dtype: np.dtype = np.complex64) -> "Phasors":
         if rng_gen is None:
             rng_gen = np.random.default_rng(seed)
         re = np.real(correlation_matrix)
@@ -32,28 +52,61 @@ class Phasors:
         return cls(amps)
 
     @staticmethod
-    def check_types(other: typing.Union["Phasors", np.generic, float, int]):
+    def check_types(other: typing.Union["Phasors", np.generic, float, int]) -> bool:
         return isinstance(other, (Phasors, np.generic, float, int))
 
-    def __add__(self, other: typing.Union["Phasors", np.generic, float, int]):
+    def __add__(self, other: typing.Union["Phasors", np.generic, float, int]) -> "Phasors":
         if not self.check_types(other):
             raise TypeError("Addition of `Phasors` should be with `Phasors` objects or scalars.")
-        return self.__amps + other
+        amps = self.__amps + other
+        new_phasors = Phasors(amps)
+        return new_phasors
 
-    def __iadd__(self, other: typing.Union["Phasors", np.generic, float, int]):
+    def __iadd__(self, other: typing.Union["Phasors", np.generic, float, int]) -> "Phasors":
         if not self.check_types(other):
             raise TypeError("Addition of `Phasors` should be with `Phasors` objects or scalars.")
         self.__amps += other
-        return self.__amps
+        return self
 
-    def __mul__(self, other: typing.Union["Phasors", np.generic, float, int]):
+    def __mul__(self, other: typing.Union["Phasors", np.generic, float, int]) -> "Phasors":
         if not self.check_types(other):
             raise TypeError("Multiplication of `Phasors` should be with `Phasors` objects or scalars.")
-        return self.__amps * other
+        amps = self.__amps * other
+        new_phasors = Phasors(amps)
+        return new_phasors
 
-    def __imul__(self, other: typing.Union["Phasors", np.generic, float, int]):
+    def __imul__(self, other: typing.Union["Phasors", np.generic, float, int]) -> "Phasors":
         if not self.check_types(other):
             raise TypeError("Multiplication of `Phasors` should be with `Phasors` objects or scalars.")
         self.__amps *= other
-        return self.__amps
+        return self
 
+    def propagate(self, shift: bool = True, inverse: bool = False, dtype: np.dtype = None) -> "Phasors":
+        if inverse:
+            self.__amps = np.fft.ifft2(self.__amps, axes=(-1, -2)).astype(dtype)
+        else:
+            self.__amps = np.fft.fft2(self.__amps, axes=(-1, -2)).astype(dtype)
+        if shift:
+            if inverse:
+                self.__amps = np.fft.ifftshift(self.__amps, axes=(-1, -2))
+            else:
+                self.__amps = np.fft.fftshift(self.__amps, axes=(-1, -2))
+        return self
+
+    def apply_mask(self, mask: np.ndarray, dtype: np.dtype = None) -> "Phasors":
+        N1 = self.N1
+        N2 = self.N2
+        if mask.shape != (N1, N2):
+            raise ValueError(f"The mask should be of shape `({N1}, {N2})`.")
+        np.multiply(self.__amps, mask, out=self.__amps, dtype=dtype)
+        return self
+
+    def phases(self, dtype: np.dtype = None) -> np.ndarray:
+        return np.angle(self.__amps).astype(dtype)
+
+    def real_amps(self, dtype: np.dtype = None) -> np.ndarray:
+        return np.abs(self.__amps).astype(dtype)
+
+    def intensities(self, dtype: np.dtype = None) -> np.ndarray:
+        real_amps = self.real_amps(dtype)
+        return real_amps * real_amps
